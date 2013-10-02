d7df99938d6625af74802b9df88cb5914168c3b6
Andrew Revinsky <arevinsky@gmail.com>
2013-10-02 23:14:23 +0400

    Colored paths.

--- static/js/prime.app.js
+++ static/js/prime.app.js
@@ -7,26 +7,62 @@
  */
 
 window.onload = function () {
-	initCanvas(document.getElementById('myCanvas'));
+	initCanvas(document.getElementById('myCanvas'), document.getElementById('number_or_colors'));
 };
 
-function initCanvas(canvas) {
+function initCanvas(canvas, colorSelector) {
+
+	var viewRatio = 10, viewOffset= {
+		x: 10, y: 10
+	};
+
 	canvas.width = 1400; // Ширина холста
 	canvas.height = 1000; // высота холста
-	canvas.addEventListener('mousemove', function (evt) {
-		var mousePos = getMousePos(canvas, evt);
-		drawPatterns(canvas, mousePos.x, mousePos.y);
+
+
+	var lastKnownCoords = { x: 1, y: 1};
+	canvas.addEventListener('mousedown', function (evt) {
+		var mousePos = getMousePos(canvas, viewRatio, viewOffset, evt),
+				colors = colorSelector.value >>> 0;
+				lastKnownCoords = mousePos;
+		drawPatterns(canvas, mousePos, colors, viewRatio, viewOffset);
+	}, false);
+
+	colorSelector.addEventListener('change', function(){
+		var colors = colorSelector.value >>> 0;
+		drawPatterns(canvas, lastKnownCoords, colors, viewRatio, viewOffset);
 	}, false);
 
 }
 
-function prepareBoard(canvas) {
+function prepareBoard(canvas, coords, ratio, viewOffset) {
 	var context = canvas.getContext('2d');
 	context.font = "10pt sans-serif";
 	context.beginPath();
 	//context.clearRect(0, 0, canvas.width, canvas.height);
 	context.fillStyle = "rgb(256,256,256)";
 	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	var xStart = viewOffset.x,
+			yStart = viewOffset.y;
+
+	var xEnd = viewOffset.x + coords.x * ratio,
+			yEnd = viewOffset.y + coords.y * ratio;
+
+	var _gcd = gcd(coords.x, coords.y),_lcm = coords.x * coords.y / _gcd;
+
+	context.beginPath();
+	context.strokeStyle = '#444';
+	context.strokeText("(" + (coords.x) + ", " + (coords.y) + "), LCM: " + _lcm + ", GCD: " + _gcd, xEnd + 20, yEnd + 20);
+
+	context.moveTo(xStart, yStart);
+	context.lineTo(xStart, yEnd);
+	context.lineTo(xEnd, yEnd);
+	context.lineTo(xEnd, yStart);
+	context.lineTo(xStart, yStart);
+
+	context.stroke();
+
 	return context;
 }
 
@@ -40,87 +76,90 @@ function lcm(a, b) {
 	return a * b / gcd(a , b);
 }
 
-function drawLine(context, style, fromX, fromY, newX, newY) {
-	var xStart = 10;
-	var yStart = 10;
-	var SizeKofX = 5;
-	var SizeKofY = 5;
-
+function drawLine(context, style, fromX, fromY, diffX, diffY, ratio, viewOffset) {
 	context.beginPath();
 	context.strokeStyle = style;
-	context.moveTo(xStart + fromX * SizeKofX, yStart + fromY * SizeKofY);
-	context.lineTo(xStart + newX * SizeKofX, yStart + newY* SizeKofY);
+	var x = viewOffset.x + (fromX + ((diffX == 1)? 0 : 1)) * ratio,
+			y = viewOffset.y + (fromY + ((diffY == 1)? 0 : 1)) * ratio;
+
+	context.moveTo(x, y + 1.5);
+	context.lineTo(
+			viewOffset.x + (fromX + ((diffX == 1 ) ? 0 : 1) + diffX) * ratio,
+			viewOffset.y + (fromY + ((diffY == 1 ) ? 0 : 1) + diffY) * ratio);
+
+	context.lineTo(x, y - 1.5);
 	context.stroke();
 }
 
-function drawPatterns(canvas, xView, yView) {
-	var context = prepareBoard(canvas);
-	var SizeKofX = 5;
-	var SizeKofY = 5;
-	var xStart = 10;
-	var yStart = 10;
+function drawPatterns(canvas, coords, colorsCount, ratio, viewOffset) {
 
+	var context = prepareBoard(canvas, coords, ratio, viewOffset);
 
-	var xMax = Math.round((xView - xStart) / SizeKofX);
-	var yMax = Math.round((yView - yStart) / SizeKofY);
+	var xMax = coords.x, yMax = coords.y;
 
+	var colors = [
+		'#ffcccc',
+		'#123eab',
+		'#ffab00',
+		'#00cc00',
+		'#6f0aaa',
+		'#ff4040',
+		'#466fd5',
+		'#ffc040',
+		'#39e639',
+		'#9d3ed5'
+	];
 
-	var x = 0, y = 0, xNew, yNew, color, xOrd, yOrd, xPar, yPar;
-	for (var iter = 0, max = lcm(xMax, yMax); iter < max; iter++) {
-		color = ((iter % 2) == 0) ? '#ff8765' : '#6512ff';
-		x = iter % xMax;
-		y = iter % yMax;
+	var color, coord, ord, even, nd, diff;
+	for (var i = 0, max = lcm(xMax, yMax); i < max; i++) {
+		color = colors[(i % (colorsCount || 2))];
 
-		xOrd = Math.floor(iter / yMax);
-		yOrd = Math.floor(iter / xMax);
+		coord = {
+			x: i % xMax,
+			y: i % yMax
+		};
 
-		xPar = (xOrd % 2) == 0;
-		yPar = (yOrd % 2) == 0;
+		ord = {
+			x: Math.floor(i / xMax),
+			y: Math.floor(i / yMax)
+		};
 
-		x = xPar ? x : xMax - x;
-		y = yPar ? y : yMax - y;
+		even = {
+			x: (ord.x % 2) == 0,
+			y: (ord.y % 2) == 0
+		};
 
-		xNew = x + (xPar ? 1 : -1);
-		yNew = y + (yPar ? 1 : -1);
+		nd = {
+			x: even.x ? coord.x : (xMax -1) - coord.x,
+			y: even.y ? coord.y : (yMax - 1) - coord.y
+		};
 
-		drawLine(context, color, x, y, xNew, yNew);
+		diff = {
+			x: (even.x ? 1 : -1),
+			y: (even.y ? 1 : -1)
+		};
+
+		drawLine(context, color, nd.x, nd.y, diff.x, diff.y, ratio, viewOffset);
 	}
-
-
-	var xStart = 10;
-	var yStart = 10;
-	var xEnd = SizeKofX * Math.round(xView / SizeKofX);
-	var yEnd = SizeKofY * Math.round(yView / SizeKofY);
-
-
-	context.beginPath();
-	//context.strokeText("("+((xEnd-xStart)/Math.abs(SizeKofX))+", "+((yEnd-yStart)/Math.abs(SizeKofY))+"), "+i, xEnd+20,yEnd+20);
-	context.strokeText("(" + (xMax) + ", " + (yMax) + "), " + max + ", " + (xMax/yMax), xEnd + 20, yEnd + 20);
-
-	context.moveTo(xStart, yStart);
-	context.lineTo(xStart, yEnd);
-	context.lineTo(xEnd, yEnd);
-	context.lineTo(xEnd, yStart);
-	context.lineTo(xStart, yStart);
-
-	context.stroke();
 }
 
-function getMousePos(canvas, evt) {
-	var obj = canvas;
-	var top = 0;
-	var left = 0;
+function getMousePos(canvas, ratio, offset, evt) {
+	var obj = canvas, top = 0, left = 0;
 	while (obj && obj.tagName != 'BODY') {
 		top += obj.offsetTop;
 		left += obj.offsetLeft;
 		obj = obj.offsetParent;
 	}
 
-	var mouseX = evt.clientX - left + window.pageXOffset;
-	var mouseY = evt.clientY - top + window.pageYOffset;
+	var mouseX = evt.clientX - left + window.pageXOffset,
+			mouseY = evt.clientY - top + window.pageYOffset;
+
+	var xMax = Math.round((mouseX - offset.x) / ratio),
+			yMax = Math.round((mouseY - offset.y) / ratio);
+
 	return {
-		x: mouseX,
-		y: mouseY
+		x: xMax,
+		y: yMax
 	};
 }
 
