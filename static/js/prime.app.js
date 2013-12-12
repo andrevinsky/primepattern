cebdbb59a58ea38890096f29adbdfe795964493f
Andrew Revinsky <arevinsky@gmail.com>
2013-12-12 05:34:27 +0400

    Added: + Drawing reworked completely

--- static/js/prime.app.js
+++ static/js/prime.app.js
@@ -31,7 +31,8 @@ $(function () {
 		skipMainDiag: $('#no_main_diag'),
 		skipSecDiag: $('#no_sec_diag'),
 		pixelate: $('#pixelate'),
-		doArrows: $('#show_directions')
+		doArrows: $('#show_directions'),
+		saveAs: $('#save_as')
 	}, {
 		canvas: { width: 1400, height: 1000 },
 		colors: 2,
@@ -456,17 +457,27 @@ function drawPatterns(view, model) {
 			alternate = model.alternate,
 			doFill = model.fill,
 			outlineOnly = model.outline,
-			colors = model.colorBank;
+			colors = model.colorBank,
+			toggleOy = model.toggleOy;
 
 	var thread = model.thread;
 	if (thread) {
 		thread.reject();
 	}
 
-	var xMax = coords.width, yMax = coords.height;
+	var xMax = coords.width,
+			yMax = coords.height;
+
+	viewOffset.yBase = model.toggleOy ? yMax : 0;
 
 	model.thread = $.Deferred(function(def){
 		prepareModel(def, xMax, yMax).done(function(patternModel){
+
+			var drawing = getDrawingContext(context, {
+				toggleOy: toggleOy,
+				ratio: ratio,
+				viewOffset: viewOffset
+			});
 
 			if (doFill) {
 				fillAsync(def, {
@@ -482,6 +493,7 @@ function drawPatterns(view, model) {
 					result: {},
 					model: patternModel,
 					mode: outlineOnly ? 'outline' : 'full',
+					drawing: drawing,
 					doIncrement: function(){
 						this.currentX++;
 						if (this.currentX < this.maxX)
@@ -500,26 +512,43 @@ function drawPatterns(view, model) {
 						return this.result;
 					},
 					run: function(){
-						var x = this.currentX, y = this.currentY;
-						var key = x + this.maxX * y, token = this.model[key];
+						var x, y, point = { x: (x = this.currentX), y: (y = this.currentY) },
+								key = x + this.maxX * y, item = this.model[key],
+								direction;
+
 						switch(this.mode) {
 							case 'outline':
-								if (!token) {
-									drawSolidBox(context, this.colorBank[this.currentColor], x, y, ratio, viewOffset);
+								if (!item) {
+									this.drawing.box({
+										color: this.colorBank[this.currentColor],
+										from: point
+									});
 								}
 								break;
 							case 'full':
-								if (token && (alternate ? ((token.idx % 2) == 0) : ((token.idx % 2) == 1))) {
-									if ((x == 0) && ((token.direction === 0)||(token.direction == 2))) {
+								if (item && (alternate ? ((item.idx % 2) == 0) : ((item.idx % 2) == 1))) {
+									direction = item.direction;
+									if ((x == 0) && ((direction === 0)||(direction == 2))) {
 										this.currentRowColor = this.currentColor = 1 - this.currentRowColor;
 									}
-									drawHalfBox(context, this.colorBank[this.currentColor], this.colorBank[1 - this.currentColor], x, y, token, ratio, viewOffset);
+//									drawHalfBox(context, this.colorBank[this.currentColor], this.colorBank[1 - this.currentColor], x, y, item, ratio, viewOffset);
+									this.drawing.halfBox({
+										color: this.colorBank[this.currentColor],
+										colorTo: this.colorBank[1 - this.currentColor],
+										from: point,
+										direction: direction
+									});
+
 									this.currentColor = 1 - this.currentColor;
-									if ((x == 0) && ((token.direction == 1)||(token.direction == 3))) {
+
+									if ((x == 0) && ((direction == 1)||(direction == 3))) {
 										this.currentRowColor = 1 - this.currentRowColor;
 									}
 								} else {
-									drawSolidBox(context, this.colorBank[this.currentColor], x, y, ratio, viewOffset);
+									this.drawing.box({
+										color: this.colorBank[this.currentColor],
+										from: point
+									});
 								}
 
 								break;
@@ -530,27 +559,42 @@ function drawPatterns(view, model) {
 				});
 
 			} else {
-				var item, color, colorKey, pixelate = model.pixelate, doArrows = model.doArrows;
 
-				viewOffset.yBase = model.toggleOy ? yMax : 0;
+				var pixelate = model.pixelate,
+						doArrows = model.doArrows,
+						skipMainDiag = model.skipMainDiag,
+						skipSecDiag = model.skipSecDiag;
+
+				var item, color, colorKey, idx, direction;
+
 				for (var i = 0, max = patternModel.length; i < max; i++) {
 					item = patternModel[i];
-					if (!item)
-						continue;
+					if (!item) continue;
 
-					var idx = item.idx;
-					var x = item.point.x,
-							y = item.point.y;
+					idx = item.idx;
+					direction = item.direction;
 
 					colorKey = (idx  % (colorsCount || 2));
 					color = ((alternate)
 							? colors[Math.floor(colorKey / 2) * 2 + (1 - (colorKey % 2))]
 							: colors[colorKey]);
 
+					if (skipMainDiag && ((direction === 0)||(direction == 2))) continue;
+					if (skipSecDiag && ((direction == 1)||(direction == 3))) continue;
+
 					if (pixelate){
-						drawSolidBox(context, color, x, y, ratio, viewOffset);
-					} else
-						drawLineNew(context, color, item.point, item.direction, ratio, viewOffset, doArrows, model.toggleOy, model.skipMainDiag, model.skipSecDiag);
+						drawing.box({
+							color: color,
+							from: item.point
+						});
+					} else {
+						drawing.line({
+							from: item.point,
+							direction: direction,
+							makeArrow: doArrows,
+							color: color
+						});
+					}
 				}
 				def.resolve();
 			}
@@ -559,9 +603,9 @@ function drawPatterns(view, model) {
 	});
 	model.thread.done(function(){
 		var uri = canvas.toDataURL('image/png');
-		view.canvasEcho.first().prop('src', uri).show().width(view.canvas.width()).height(view.canvas.height()).show();
+		view.canvasEcho.first().prop('src', uri).attr('download', 'IMG_' + (new Date() - 0) + '.png').show().width(view.canvas.width()).height(view.canvas.height()).show();
+		view.saveAs.attr('href', uri).attr('download', 'IMG_' + (new Date() - 0) + '.png');
 		view.canvas.hide();
-
 	}).always(function(){
 		delete model.thread;
 	});
@@ -574,151 +618,169 @@ function fillAsync(thread, initialState){
 	return def.promise();
 }
 
-function drawLineNew(context, style, boxCoords, direction, ratio, viewOffset, doArrows, toggleOy, skipMainDiag, skipSecDiag) {
-	context.beginPath();
-	context.strokeStyle = style;
-
-	var makeLine = bindArgs(context, makeVect);
-
-	var rotApos = feedArray(null, curry(null, rot, direction)),
-			toWorld = feedArray(null, curry(null, transpose, ratio, ratio, viewOffset.x, viewOffset.y)),
-			toWorldDiff = feedArray(null, curry(null, transpose, ratio, ratio, 0, 0));
-
-	if (toggleOy) {
-		toWorld = feedArray(null, curry(null, transpose, ratio, -ratio, viewOffset.x, viewOffset.y + (ratio * viewOffset.yBase)));
-		toWorldDiff = feedArray(null, curry(null, transpose, ratio, -ratio, 0, 0));
-	}
-
-	var vector = rotApos([1, 1]);
-	var basePoint = add([boxCoords.x, boxCoords.y], [[0,0], [1, 0], [1, 1], [0, 1]][direction]);
-
-	if (skipSecDiag && ((direction == 1)||(direction == 3))) { return; }
-	if (skipMainDiag && ((direction === 0)||(direction == 2))) { return; }
-
-	if (!doArrows) {
-		makeLine(toWorld(basePoint), toWorldDiff(vector));
-	} else {
-		makeLine(toWorld(basePoint), toWorldDiff(vector));
-		var endPin = add(basePoint, vector),
-				v1 = rot(direction, -(7/12),-(5/12)),
-				v2 = rot(direction, -(5/12),-(7/12));
-		makeLine(toWorld(endPin), toWorldDiff(v1));
-		makeLine(toWorld(endPin), toWorldDiff(v2));
-		makeLine(toWorld(add(endPin, v1)), toWorldDiff(add(v2, neg(v1))));
-	}
-
-	context.stroke();
-}
-
-function makeVect(from, vect) {
-	var fromX = from[0], fromY = from[1], vX = vect[0], vY = vect[1];
-	this.moveTo(fromX, fromY);
-	this.lineTo(fromX + vX, fromY + vY);
-}
-
-function transpose(kX, kY, offsetX, offsetY, x, y) {
-	return [offsetX + x * kX, offsetY + y * kY];
-}
-
-function add(v1, v2) {
-	return [v1[0] + v2[0], v1[1] + v2[1]];
-}
-function neg(v1) {
-	return [-v1[0], -v1[1]];
-}
-
-function rot(degShort, x, y) {
-	var sD, cD;
-	switch(degShort % 4) {
-		case 0: // 45 deg;
-			sD = 0; cD = 1;
-			break;
-		case 1: // 45 + 90 deg
-			sD = 1; cD = 0;
-			break
-		case 2: // 45 + 180 deg
-			sD = 0; cD = -1;
-			break;
-		case 3: // 45 + 270 deg
-			sD = -1; cD = 0;
-			break;
-	}
-	return [x * cD - y * sD, y * cD + x * sD];
-}
-
-function bindArgs(context, func, Tfunc) {
-	var isTFuncPresent = arguments.length == 3;
-	if (isTFuncPresent)
-		return function(){
-			return func.apply(context || this, Tfunc.apply(this, arguments));
-		};
-	else {
-		return function(){
-			return func.apply(context || this, arguments);
-		};
-	}
-}
-
-function curry(context, func, _args) {
-	var args = Array.prototype.slice.call(arguments, 2);
+function wrapAfter(func1, func2, context) {
 	return function(){
-		var args1 = args.concat(Array.prototype.slice.apply(arguments));
-		return func.apply(context || this, args1);
+		return func2.call(context || this, func1.apply(context || this, arguments));
 	};
 }
 
-function feedArray(context, func) {
-	return function(args) {
-		return func.apply(context || this, args);
+function getDrawingContext(context, model) {
+	return {
+		box: function(o) {
+			var boxCoords = o.from,
+					style = o.color;
+
+			var basePoint = [boxCoords.x, boxCoords.y]; //add([boxCoords.x, boxCoords.y], [[0,0], [1, 0], [1, 1], [0, 1]][0]);
+
+			this.begin()
+					.setFillStyle(style)
+					.makeBox(this.toWorld(basePoint), this.toWorldDiff([1, 1]))
+					.stroke();
+		},
+		halfBox: function(o){
+			var boxCoords = o.from,
+					direction = o.direction,
+					style = o.color,
+					style2 = o.colorTo;
+
+			var isMainDiag = ((direction == 0)||(direction == 2)),
+					basePoint = isMainDiag ? [boxCoords.x, boxCoords.y] : add([boxCoords.x, boxCoords.y], [1, 0]),
+					v1, v2, v3;
+
+			if (isMainDiag) {
+				v1 = [1, 1]; v2 = [0,-1]; v3 = [-1,0];
+			} else {
+				v1 = [-1, 1]; v3 = [0, -1]; v2 = [1, 0];
+			}
+
+			this.begin().setFillStyle(style).setStrokeStyle('transparent')
+					.makePoly(this.toWorld(basePoint), this.toWorldDiff(v1), this.toWorldDiff(v3))
+					.close().fill();
+
+			this.begin().setFillStyle(style2).setStrokeStyle('transparent')
+					.makePoly(this.toWorld(basePoint), this.toWorldDiff(v1), this.toWorldDiff(v2))
+					.close().fill();
+		},
+		line: function(o){
+			var boxCoords = o.from,
+					direction = o.direction,
+					makeArrow = o.makeArrow,
+					style = o.color;
+
+			var vector = rot(direction, 1, 1),
+					basePoint = add([boxCoords.x, boxCoords.y], [[0,0], [1, 0], [1, 1], [0, 1]][direction]);
+
+			this.begin().setStrokeStyle(style);
+
+			if (!makeArrow) {
+				this.makeLine(this.toWorld(basePoint), this.toWorldDiff(vector));
+			} else {
+				var endPin = add(basePoint, vector),
+						v1 = rot(direction, -(7/12),-(5/12)),
+						v2 = rot(direction, -(5/12),-(7/12));
+				this.makeLine(this.toWorld(basePoint), this.toWorldDiff(vector))
+						.makeLine(this.toWorld(endPin), this.toWorldDiff(v1))
+						.makeLine(this.toWorld(endPin), this.toWorldDiff(v2))
+						.makeLine(this.toWorld(add(endPin, v1)), this.toWorldDiff(add(v2, neg(v1))));
+			}
+			this.stroke();
+		},
+		begin: wrapAfter(function(){ context.beginPath(); }, returnThis),
+		stroke: wrapAfter(function(){ context.stroke(); }, returnThis),
+		fill: wrapAfter(function(){ context.fill(); }, returnThis),
+		close: wrapAfter(function(){ context.closePath(); }, returnThis),
+		setStrokeStyle: wrapAfter(function(style) { context.strokeStyle = style; }, returnThis),
+		setFillStyle: wrapAfter(function(style) { context.fillStyle = style;; }, returnThis),
+		makeLine: wrapAfter(bindArgs(context, makeVect), returnThis),
+		makeBox: wrapAfter(bindArgs(context, makeRect), returnThis),
+		makePoly: wrapAfter(bindArgs(context, makePolygon), returnThis),
+		rotate: feedArray(null, rot),
+		toWorld: model.toggleOy
+				? feedArray(null, curry(null, transpose, model.ratio, -model.ratio, model.viewOffset.x, model.viewOffset.y + (model.ratio * model.viewOffset.yBase)))
+				: feedArray(null, curry(null, transpose, model.ratio, model.ratio, model.viewOffset.x, model.viewOffset.y)),
+		toWorldDiff: model.toggleOy
+				? feedArray(null, curry(null, transpose, model.ratio, -model.ratio, 0, 0))
+				: feedArray(null, curry(null, transpose, model.ratio, model.ratio, 0, 0))
 	};
-}
 
-function drawSolidBox(context, style, x, y, ratio, viewOffset) {
-	context.beginPath();
-	context.fillStyle = style;
-	context.fillRect(viewOffset.x + x * ratio, viewOffset.y + y * ratio, ratio, ratio);
-	context.stroke();
-}
-
-function drawHalfBox(context, currentColor, supplementaryColor, x, y, token, ratio, viewOffset) {
-	if ((token.direction == 0)||(token.direction == 2)) {
-		context.beginPath();
-		context.fillStyle = currentColor;
-		context.strokeStyle = 'transparent'; //currentColor;
-		context.moveTo(viewOffset.x + x * ratio, viewOffset.y + y * ratio);
-		context.lineTo(viewOffset.x + x * ratio + ratio, viewOffset.y + y * ratio);
-		context.lineTo(viewOffset.x + x * ratio , viewOffset.y + y * ratio + ratio);
-		context.closePath();
-		context.fill();
-
-		context.beginPath();
-		context.fillStyle = supplementaryColor;
-		context.strokeStyle = 'transparent'; //currentColor;
-		context.moveTo(viewOffset.x + x * ratio  + ratio, viewOffset.y + y * ratio);
-		context.lineTo(viewOffset.x + x * ratio + ratio, viewOffset.y + y * ratio + ratio);
-		context.lineTo(viewOffset.x + x * ratio, viewOffset.y + y * ratio  + ratio);
-		context.closePath();
-		context.fill();
-	} else {
-		context.beginPath();
-		context.fillStyle = currentColor;
-		context.strokeStyle = 'transparent'; //currentColor;
-		context.moveTo(viewOffset.x + x * ratio, viewOffset.y + y * ratio);
-		context.lineTo(viewOffset.x + x * ratio , viewOffset.y + y * ratio + ratio);
-		context.lineTo(viewOffset.x + x * ratio+ ratio, viewOffset.y + y * ratio  + ratio);
-		context.closePath();
-		context.fill();
-
-		context.beginPath();
-		context.fillStyle = supplementaryColor;
-		context.strokeStyle = 'transparent'; //currentColor;
-		context.moveTo(viewOffset.x + x * ratio, viewOffset.y + y * ratio);
-		context.lineTo(viewOffset.x + x * ratio + ratio, viewOffset.y + y * ratio);
-		context.lineTo(viewOffset.x + x * ratio + ratio, viewOffset.y + y * ratio  + ratio);
-		context.closePath();
-		context.fill();
-
+	function returnThis() { return this; }
+	function makeRect(from, vect) {
+		var fromX = from[0], fromY = from[1], vX = vect[0], vY = vect[1];
+		this.fillRect(fromX, fromY, vX, vY);
 	}
+	function makeVect(from, vect) {
+		var fromX = from[0], fromY = from[1], vX = fromX + vect[0], vY = fromY + vect[1];
+		this.moveTo(fromX, fromY);
+		this.lineTo(vX, vY);
+	}
+	function makePolygon(from) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		var fromX = from[0], fromY = from[1], vX = fromX, vY= fromY, vect;
+		this.moveTo(fromX, fromY);
+		for (; vect = args.shift();) {
+			vX += vect[0];
+			vY += vect[1];
+			this.lineTo(vX, vY);
+		}
+	}
+
+	function transpose(kX, kY, offsetX, offsetY, x, y) {
+		return [offsetX + x * kX, offsetY + y * kY];
+	}
+
+	function add(v1, v2) {
+		return [v1[0] + v2[0], v1[1] + v2[1]];
+	}
+	function neg(v1) {
+		return [-v1[0], -v1[1]];
+	}
+
+	function rot(degShort, x, y) {
+		var sD, cD;
+		switch(degShort % 4) {
+			case 0: // 45 deg;
+				sD = 0; cD = 1;
+				break;
+			case 1: // 45 + 90 deg
+				sD = 1; cD = 0;
+				break
+			case 2: // 45 + 180 deg
+				sD = 0; cD = -1;
+				break;
+			case 3: // 45 + 270 deg
+				sD = -1; cD = 0;
+				break;
+		}
+		return [x * cD - y * sD, y * cD + x * sD];
+	}
+
+	function bindArgs(context, func, Tfunc) {
+		var isTFuncPresent = arguments.length == 3;
+		if (isTFuncPresent)
+			return function(){
+				return func.apply(context || this, Tfunc.apply(this, arguments));
+			};
+		else {
+			return function(){
+				return func.apply(context || this, arguments);
+			};
+		}
+	}
+
+	function curry(context, func, _args) {
+		var args = Array.prototype.slice.call(arguments, 2);
+		return function(){
+			var args1 = args.concat(Array.prototype.slice.apply(arguments));
+			return func.apply(context || this, args1);
+		};
+	}
+
+	function feedArray(context, func) {
+		return function(args) {
+			return func.apply(context || this, args);
+		};
+	}
+
 }
 
 function getMousePos(canvas, model, evt) {
